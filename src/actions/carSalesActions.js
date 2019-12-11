b3c8bb3ff55f5567074e88730cdd0aa001b31fa2
David Tauraso <david318@me.com>
2019-12-11 13:37:31 -0800

    Cleanup some issues while setting up redux

---
+++ src/actions/carSalesActions.js
@@ -0,0 +1,10 @@
+
+// action creator
+
+export const addAditionalFeature = () => {
+    // action
+    return { type: 'ADD_ADDITIONAL_FEATURE'}
+};
+
+// UNDER THE HOOD stuff
+// dispatch(someFunc());
